61fcccd9e674cf0d02db466143ecc9eaa6740d49
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-03-28 05:51:16 +0900

    day 8 final project result

---
+++ basic_final_projects/07_caesar_cipher.py
@@ -0,0 +1,46 @@
+# import asci art
+from caesar import art
+
+# ahpabet list
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+            'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+            'y', 'z']  
+
+# call caesar fucntion
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        # fixed the code to keep the number/symbol/space when the text is encoded/decoded
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# print the logo from art.py when the program starts.
+print(art.logo)
+
+# figure out a way to ask the user if they want to restart the cipher program?
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # What if the user enters a shift that is greater than the number of letters in the alphabet?
+    # Try running the program and entering a shift number of 45.
+    # Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Good Bye")
