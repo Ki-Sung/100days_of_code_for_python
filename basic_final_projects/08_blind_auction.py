3192c4c33635b92cb1093a8adc2dd652a83a806e
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-03-31 22:35:35 +0900

    day 9 final project done

---
+++ basic_final_projects/08_blind_auction.py
@@ -0,0 +1,31 @@
+import os 
+from auction import auction_art
+
+print(auction_art.logo)
+print("Welcome to the secret auction program.")
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    #bidding_record = {"name": 123, "name": 234}
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+    
+while not bidding_finished:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'?\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        os.system("clear")
