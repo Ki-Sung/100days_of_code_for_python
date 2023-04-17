807a58a7d0be72bdd2d7bf1fb07d9879b37f4605
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-04-17 22:14:43 +0900

    day 14 done

--- basic_final_projects/12_higher_lower.py
+++ basic_final_projects/12_higher_lower.py
@@ -3,38 +3,62 @@ import random
 from higher_lower import art
 from higher_lower import game_data
 
-# 1. 로고 출력 
-print(art.logo)              # 시작 로고 
+# Format the account data into printable format.
+def format_data(account):
+    """Format the account data and return the printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 
-# 2. 팔로우 데이터 불러오기 
-data = game_data.data
+## Use if statement to check if user is correct
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
-game_over = False
+# Display art
+print(art.logo)
+
 score = 0
-while not game_over:
-    # 3. 초반에 랜덤하게 두 대결 상대를 출력 
-    compare = random.choice(data)
-    print("Compare A: " + compare['name'] + ", " + compare['description'] + ", " + "from " + compare['country'])
-    print(art.vs)                # vs 로고 
-    against = random.choice(data)
-    print("Against B: " + against['name'] + ", " + against['description'] + ", " + "from " + against['country'])
+game_should_countinue = True
+account_b = random.choice(game_data.data)
+
+# Make the game repeatable.
+while game_should_countinue:
+    # Generate a random account from the game data.
     
-    # 4. 선택할 수 있게 input 할 사항 넣기
-    choice = input("Who has more followers? Type 'A' or 'B': ")
-    if choice == 'A':
-        choice_result = compare['follower_count']
-        print(choice_result)
-    else:
-        choice_result = against['follower_count']
-        print(choice_result)
-        
-    if choice_result > compare['follower_count']:
+    # Making account at position B become the next account at position A
+    account_a = account_b
+    account_b = random.choice(game_data.data)
+    
+    if account_a == account_b:
+        account_b = random.choice(game_data.data)
+
+    print(f"Compare A: {format_data(account_a)}")
+    print(art.vs)
+    print(f"Against B: {format_data(account_b)}")
+
+    # Ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # Check if user is correct.
+    ## Get follower count of each account.
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    
+    # Clear the screen between rounds.
+    os.system("clear")
+    print(art.logo)
+    
+    # Give user feedback on their guess.
+    # Score keeping
+    if is_correct:
         score += 1
-        print(f"you got it Current score: {score}")
-    elif choice_result > against['follower_count']:
-        score += 1
-        print(f"you got it Current score: {score}")
+        print(f"You're right! Current score: {score}.")
     else:
-        os.system('clear')
-        print(f"Sorry, that's wrong. Final score:{score}")
-        game_over = True
+        game_should_countinue = False
+        print(f"Sorry, that's wrong. Final score: {score}.")
