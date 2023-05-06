b680b4d78c8de355832601d5e8b920fd638e3f9c
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-06 18:49:46 +0900

    day 17 final project 3

--- intermediate_final_projects/03_quiz_quiz.py
+++ intermediate_final_projects/03_quiz_quiz.py
@@ -1,5 +1,6 @@
 from quiz.question_mode import Question     # 퀴즈 문제에 대한 Question 클래스 불러오기 
-from quiz.data import question_data         # 퀴즈 문제 및 정답에 대한 data
+from quiz.data import question_data         # 퀴즈 문제 및 정답에 대한 data 불러오기 
+from quiz.quiz_brain import QuizBrain       # 퀴즈의 모든 질문과 퀴즈의 기능을 위한 QuizBrain 클래스 불러오기
 
 # 퀴즈 문제 및 정답 정보 처리 - quiz 폴더 안 data.py 안에 있는 딕셔너리 형태 데이터 불러오기
 question_bank = []                                                  # 문제, 문제 정답을 담기 위한 빈 List  
@@ -9,7 +10,9 @@ for question in question_data:                                      # 반복문 
     new_question = Question(question_text, question_answer)         # Question 클래스 객체 생성 및 파라미터 설정 
     question_bank.append(new_question)                              # 뽑은 정보들 question_bank에 저장
 
-# 결과 출력    
-print(question_bank[0].text)
-print(question_bank[0].answer)
+# 퀴즈의 모든 질문과 퀴즈의 기능을 제어하기
+quiz = QuizBrain(question_bank)        # QuizBrain 클래스 객체 생성 및 파미터 설정 
 
+# 퀴즈가 남아있는지 확인 기능 제어
+while quiz.stil_has_question():        # 문제가 남아있는지 체크 
+    quiz.next_question()               # 만약 남아있다면(True이면) 다음 질문으로 넘어가고, 남아있지 않다면(False이면) 반복문 종료 
---
+++ intermediate_final_projects/quiz/quiz_brain.py
@@ -0,0 +1,27 @@
+# QuizeBrain -> 퀴즈의 모든 질문과 퀴즈의 기능을 위한 Class 
+# 기능 1: 사용자에게 다음 질문을 제시 
+# 기능 2: 퀴즈의 정답을 맞췄는지 여부 확인 
+# 기능 3: 퀴즈의 마지막 문제까지 도달했는지 여부 확인 
+# 필요 속성: quetion_number - 퀴즈 문제 순서 / question_list - 질문 리스트 
+# 필요한 메소드 1: next_question() - 현재 퀴즈 질문 호출 및 입력할 정답 호출 기능
+# 필요한 메소드 2: still_has_questions() - 뒤에 퀴즈가 남았는지 여부를 확인하는 기능
+
+class QuizBrain:                                    # QuizBrain 이라는 클래스 생성 
+    # 초기화 메소드 
+    def __init__(self, q_list):                     # question_number(기본값이 0이기 때문에 여기서 생략), question_list, 두 속성을 초기화 하는 init 메소드 (입력 값 입력)
+        self.question_number = 0                    # 속성 설정 1. 문제 번호를 위한 "question_number"
+        self.question_list = q_list                 # 속성 설정 2. quiz 데이터를 뽑기 위한 "question_list"
+    
+    # still_has_questions() - 뒤에 퀴즈가 남았는지 여부를 확인하는 기능
+    def stil_has_question(self):
+        return self.question_number < len(self.question_list)       # 아래의 구조를 하나로 작성 
+        # if self.question_number < len(self.question_list):                     # 리스트에 있는 질문의 개수만큼 반복 실행되길 원함 - question_number가 question_list의 길이 보다 적다면,
+        #     return True                                                        # True를 반환 
+        # else:                                                                  # question_number가 question_list의 길이 보다 크다면,
+        #     return False                                                       # False를 반환 
+        
+    # next_question 메소드 - question_list에서 현재 질문 번호의 항목을 가져오고, input 함수를 이용해서 입력한 정답을 호출하는 기능
+    def next_question(self):
+        current_question = self.question_list[self.question_number]                     # 현재 퀴즈 질문 - question_list[question_number]
+        self.question_number += 1                                                       # 문제 번호 더하기 - 해당 설정을 하지 않으면, Q.0으로 넘버링이 됨 
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")      # 입력할 정답 호출 
