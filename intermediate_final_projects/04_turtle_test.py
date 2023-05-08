451608b9d52dd9ecea92a85205368f3665a4a976
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-08 10:01:14 +0900

    day 18 draw random step

--- intermediate_final_projects/04_turtle_test.py
+++ intermediate_final_projects/04_turtle_test.py
@@ -49,23 +49,44 @@
 #     turtle.forward(5)    # 앞으로 5번 (빈칸 기준 앞으로 5번 전진)
 #     turtle.pendown()     # 펜을 아래로 내리고 (다시 선 그릴 준비)
 
-# 04. 한번에 삼각형, 사각형, 오각형, 육각형, 칠각형, 팔각형, 구각형, 십각형 만들기
+# # 04. 한번에 삼각형, 사각형, 오각형, 육각형, 칠각형, 팔각형, 구각형, 십각형 만들기
+# from turtle import Turtle
+# import random
+
+# turtle = Turtle() # 객체 선언 
+
+# # 색상 list 담기 
+# colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+# # 도형을 그리기 위한 기능 함수 
+# def drew_shape(num_sides):
+#     angle = 360 / num_sides     # 각 도형의 각도를 나누는 방정식 
+#     for _ in range(num_sides):
+#         turtle.forward(100)     # 앞으로 100 
+#         turtle.right(angle)     # 나눈 각도 대로 오른쪽으로 전환 
+
+# # 도형 그리기 
+# for shape_side_n in range(3, 11):   # 삼각형에서 십각형 까지 그리기 위해 범위를 3에서 10까지 설정
+#     turtle.color(random.choice(colors))    # 지정한 colors 리스트를 랜덤하게 뽑기 
+#     drew_shape(shape_side_n)            # 도형 그리기 
+    
+# 05. 무작위 행보 
+import random 
 from turtle import Turtle
-import random
 
-turtle = Turtle() # 객체 선언 
-
-# 색상 list 담기 
+# 랜덤으로 색상을 구현하기 위한 색상 리스트 
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-# 도형을 그리기 위한 기능 함수 
-def drew_shape(num_sides):
-    angle = 360 / num_sides     # 각 도형의 각도를 나누는 방정식 
-    for _ in range(num_sides):
-        turtle.forward(100)     # 앞으로 100 
-        turtle.right(angle)     # 나눈 각도 대로 오른쪽으로 전환 
+# 객체 생성 
+turtle = Turtle()
 
-# 도형 그리기 
-for shape_side_n in range(3, 11):   # 삼각형에서 십각형 까지 그리기 위해 범위를 3에서 10까지 설정
-    turtle.color(random.choice(colors))    # 지정한 colors 리스트를 랜덤하게 뽑기 
-    drew_shape(shape_side_n)            # 도형 그리기 
+# 각종 설정 
+turtle.pensize(15)   # 선 굵기 설정 
+turtle.speed("normal")   # turtle 속도 설정
+directions = [0, 90, 180, 270]  # 방향 설정 
+
+# 무작위 행보 실행 
+for _ in range(150):
+    turtle.color(random.choice(colors))              # 색상 리스트 랜덤으로 호출
+    turtle.setheading(random.choice(directions))    # turtle이 바라보는 방향(directions)을 각도 만큼 변경
+    turtle.forward(30)      # 30만큼 앞으로 이동
