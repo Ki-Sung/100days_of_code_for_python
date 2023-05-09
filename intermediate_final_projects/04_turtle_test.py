c3f8b669c6acae044931ae8c7c7f9971839c17b1
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-09 21:00:50 +0900

    day 18 make random color

--- intermediate_final_projects/04_turtle_test.py
+++ intermediate_final_projects/04_turtle_test.py
@@ -72,13 +72,23 @@
     
 # 05. 무작위 행보 
 import random 
-from turtle import Turtle
-
-# 랜덤으로 색상을 구현하기 위한 색상 리스트 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+import turtle as t
 
 # 객체 생성 
-turtle = Turtle()
+turtle = t.Turtle()
+# RGB 값 0~255를 생성하기 위해 colormode 선언 
+t.colormode(255)
+
+# 랜덤 컬러 함수 설정 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+# 랜덤으로 색상을 구현하기 위한 색상 리스트 
+# colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
 # 각종 설정 
 turtle.pensize(15)   # 선 굵기 설정 
@@ -87,6 +97,6 @@ directions = [0, 90, 180, 270]  # 방향 설정
 
 # 무작위 행보 실행 
 for _ in range(150):
-    turtle.color(random.choice(colors))              # 색상 리스트 랜덤으로 호출
+    turtle.color(random_color())              # 색상 리스트 랜덤으로 호출
     turtle.setheading(random.choice(directions))    # turtle이 바라보는 방향(directions)을 각도 만큼 변경
     turtle.forward(30)      # 30만큼 앞으로 이동
