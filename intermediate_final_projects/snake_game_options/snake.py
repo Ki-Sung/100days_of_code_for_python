dca7c629de3a730b8185dbd723250d794e708455
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-05-26 08:28:26 +0900

    day 21 replace code for detect collision with tail

--- intermediate_final_projects/snake_game_options/snake.py
+++ intermediate_final_projects/snake_game_options/snake.py
@@ -19,10 +19,10 @@ class Snake:
     # 위에 init 함수에 지정한 create_snake() 메소드 생성 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            self.add_segment(position)     # 아라에 있는 add_segment 메소드 불러오기
+            self.add_segment(position)     # 아라에 있는 add_segment 메소드 불러와서 전달
     
-    # 뱀의 길이가 늘어나기 위한 tutle 설정이 들어간 add_segment() 메소드 
-    def add_segment(self, position):
+    # 뱀의 길이가 늘어나기 위한 tutle 설정이 들어간 add_segment() 메소드 - 세그먼트가 추가될 위치가 반드시 필요
+    def add_segment(self, position):             
         new_segment = Turtle(shape="square")      # turtle 모양 설정 
         new_segment.color("white")                # tuttle 색상 설정 
         new_segment.penup()                       # 선을 없애기 위해 penup 설정 
@@ -32,7 +32,7 @@ class Snake:
     # 먹이를 먹을 때 뱀의 길이가 늘어나는 extend 메소드 생성
     def extend(self):
         # 새로운 snake(segment) 추가
-        self.add_segment(self.segments[-1].position())    # segment 마지막에 추가 
+        self.add_segment(self.segments[-1].position())    # segment 위치를 마지막에 추가 
         
     # snake의 움직임 설정인 move 메소드 생성     
     def move(self):
