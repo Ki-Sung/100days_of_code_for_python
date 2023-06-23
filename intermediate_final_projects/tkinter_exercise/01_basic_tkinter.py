053bb0b851bf8840ecc1ab0428da63dc1ac6ff1d
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-06-23 19:05:44 +0900

    day 27 basic tkinter

--- intermediate_final_projects/tkinter_exercise/01_basic_tkinter.py
+++ intermediate_final_projects/tkinter_exercise/01_basic_tkinter.py
@@ -11,6 +11,7 @@ window.minsize(width=500, height=300)  # 실행시 기본사이즈 설정
 my_label = tkinter.Label(text="I am Label!!", font=("Arial", 24, "bold"))
 
 # 컴포넌트(my_label)가 스크린에 보여지기 위해 명시하기 (꼭 해야 window에 출력됨)
+my_label.pack()                     # 지정하지 않으면 맨위 중앙에 명시 
 # my_label.pack(side="left")        # 지정한 label 왼쪽으로 명시 
 # my_label.pack(side="bottom")      # 지정한 label 맨 아래로 명시 
 # my_label.pack(expand=True)        # window 기준 정중에 명시 
