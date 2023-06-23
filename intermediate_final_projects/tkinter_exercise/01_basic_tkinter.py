1588b7196558c8a9d9e217a1250a70a17644f7c0
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-06-23 19:04:10 +0900

    day 27 basic tkinter

---
+++ intermediate_final_projects/tkinter_exercise/01_basic_tkinter.py
@@ -0,0 +1,19 @@
+# 모듈 볼러오기 - tkinter
+import tkinter
+
+# 윈도우 만들기 
+window = tkinter.Tk()   # 객체 선언 
+
+window.title("My First GUI Program")   # window 타이틀 지정
+window.minsize(width=500, height=300)  # 실행시 기본사이즈 설정
+
+# Label - text, font(폰트명, 크기, 굴기설정 등)설정 가능
+my_label = tkinter.Label(text="I am Label!!", font=("Arial", 24, "bold"))
+
+# 컴포넌트(my_label)가 스크린에 보여지기 위해 명시하기 (꼭 해야 window에 출력됨)
+# my_label.pack(side="left")        # 지정한 label 왼쪽으로 명시 
+# my_label.pack(side="bottom")      # 지정한 label 맨 아래로 명시 
+# my_label.pack(expand=True)        # window 기준 정중에 명시 
+
+
+window.mainloop()       # tkinter 스크린이 유지되도록 설정 
