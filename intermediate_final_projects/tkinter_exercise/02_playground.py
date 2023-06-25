f676fc72e6a340c2b604ed2b8901022fe82eb099
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-06-25 21:22:26 +0900

    day 27 study use keyword arguments

--- intermediate_final_projects/tkinter_exercise/02_playground.py
+++ intermediate_final_projects/tkinter_exercise/02_playground.py
@@ -23,3 +23,32 @@ def add(*args):           # 입력한 인수로 함수 선언 (튜플 형식)
 # 1. 함수안에 들어갈 인수 설정 
 total = add(5, 4, 6, 7) 
 print(total)
+
+## keyword argument를 이용하여 계산기 함수 만들기 (**kwargs)
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+    
+calculate(2, add=3, multiply=5)
+
+## keyword argument 클래스로 활용 - 대부분 파이썬 모듈이 이런 형식이다. 
+# 차 정보에 대한 클래스 
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get('make')
+        self.model = kw.get('model')
+        self.color = kw.get('color')
+        self.seats = kw.get('seats')
+        
+# 클래스 선언 후 출력 
+my_car = Car(make = "Nissan")
+print(my_car.make)
+
+my_car = Car(make = "Nissan", model="GT-R")
+print(my_car.model)
+
+my_car = Car(color="yellow")
+print(my_car.color)
