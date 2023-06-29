e84d1602a619f9bfaa48c5ae02683e5034882c49
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-06-29 21:06:58 +0900

    day 28 add timer count down function

--- intermediate_final_projects/12_pomodoro_timer.py
+++ intermediate_final_projects/12_pomodoro_timer.py
@@ -2,7 +2,7 @@
 
 # 모듈 불러오기 
 from tkinter import *
-
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 # color
 PINK = "#e2979c"
@@ -19,14 +19,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+# 타이머 함수 설정 
+def start_timer():
+    
+    count_down(5 * 60)                       # 카운트다운 기능 호출  (시작 시간 설정 - 초)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# count_down 함수 설정 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
-    if count > 0:
-        window.after(1000, count_down, count - 1)                   # 1초 뒤에 say_somthing 함수를 이용하여 3, 4, 5 추출 
-
-
+    # 카운트다운 시작 시간 설정 
+    count_min = math.floor(count / 60)                                    # 분 계산 - count(초) / 60(초)
+    count_sec = count % 60                                                # 초 계산 - count(초) % 60(초) -> 나머지 
+    
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")        # canvas.itemconfig() - tkinter를 동적으로 나타낼 수 있는 메소드 
+    if count > 0:                                                         # 만약 count가 0 보다 크면
+        window.after(1000, count_down, count - 1)                         # 1초 뒤에 say_somthing 함수를 이용하여 카운트다운 호출 (1씩 줄어듬)
 # ---------------------------- UI SETUP ------------------------------- #
 # 기본 설정 
 window = Tk()                                   # tkinter 객체 선언 
@@ -42,12 +49,10 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)         
 tomato_img = PhotoImage(file='./img/tomato.png')                                         # 이미지 불러오기 
 canvas.create_image(100, 112, image=tomato_img)                                          # 이미지 생성 - 생성시 크기 설정 
 timer_text = canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, "bold"))   # 이미지 위에 들어갈 타이머(text) 설정 
-canvas.grid(column=1, row=1)                                   # 설정한 cancas 명시 
-
-count_down(5)
+canvas.grid(column=1, row=1)                                                             # 설정한 cancas 명시 
 
 # button 1 - start 
-start_button = Button(text="Start", bg=YELLOW, highlightthickness=0)                     # start 버튼 설정 - 버튼 이름, 색상, 두깨 설정 
+start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)                     # start 버튼 설정 - 버튼 이름, 색상, 두깨 설정 
 start_button.grid(column=0, row=2)                                                       # start 버튼 명시
 
 # button 2 - Reset 
