1b6cc62fed136f8e793c005908d3c8428a35b56e
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-07-30 23:33:33 +0900

    day34 make UI for quizzler

--- intermediate_final_projects/18_quizzler_app.py
+++ intermediate_final_projects/18_quizzler_app.py
@@ -1,6 +1,7 @@
 from quiz_module.question_model import Question
 from quiz_module.data import question_data
 from quiz_module.quiz_brain import QuizBrain
+from quiz_module.ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -11,9 +12,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- intermediate_final_projects/quiz_module/ui.py
+++ intermediate_final_projects/quiz_module/ui.py
@@ -1,2 +1,25 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+        
+        self.canvas = Canvas(width=300, height=250, bg="white") 
+        self.question_text = self.canvas.create_text(
+                                                    150,
+                                                    125, 
+                                                    text="Some Question Text", 
+                                                    fill=THEME_COLOR,
+                                                    font=("Arial", 20, "italic")
+                                                    )
+        self.canvas.grid(row=1, column=0, columnspan=2)
+        
+        self.window.mainloop()
