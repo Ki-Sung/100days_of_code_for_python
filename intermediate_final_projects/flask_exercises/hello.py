2b4ee2b0a5d1e396e25de6ea0b29f798bd0045fb
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-10-16 21:31:23 +0900

    day 55 start & usr randering

--- intermediate_final_projects/flask_exercises/hello.py
+++ intermediate_final_projects/flask_exercises/hello.py
@@ -2,13 +2,30 @@
 from flask import Flask
 app = Flask(__name__)
 
+# main 페이지 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+# bye 페이지 
 @app.route("/bye")
 def say_bye():
     return 'Bye'
 
+# username 페이지 - URL에 변수 섹션 추가
+# @app.route("/username/<name>")
+# def greet1(name):
+#     return f"Hello, My name is {name}"
+
+# username 페이지 - URL에 컨버터 기능 추가 1
+# @app.route("/username/<path:name>/")
+# def greet1(name):
+#     return f"Hello, My name is {name}"
+
+# username 페이지 - URL에 컨버터 기능 추가 2 
+@app.route("/username/<name>/<int:number>")
+def greet1(name, number):
+    return f"Hello, My name is {name}, you are {number} years old"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)       # 실행시 디버그 모드 활성화
