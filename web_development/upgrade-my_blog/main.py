5754b23bfb5bc1d4517fefb9c47a43414ef7eab8
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-11-19 22:45:14 +0900

    day 59 done, post blog page rendering

--- web_development/upgrade-my_blog/main.py
+++ web_development/upgrade-my_blog/main.py
@@ -5,12 +5,6 @@ from flask import Flask, render_template
 # 블로그 포스트 내용 API 제어 
 posts = requests.get("https://api.npoint.io/37689f9426dd018a0fd4").json()   # API 엔드포인트의 JSON 데이터 가져오기 
 
-# 빈 객체 생성  
-# post_objects = []
-# for post in posts:
-#     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-#     post_objects.append(post_obj)
-
 # 플라스크 객체 선언 
 app = Flask(__name__)
 
@@ -29,6 +23,14 @@ def about_blog():
 def contact_me():
     return render_template("contact.html")
 
+# 블로그 포스트 페이지 구조 
+@app.route('/post/<int:index>')
+def blog_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
