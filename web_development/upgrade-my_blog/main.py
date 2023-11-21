893ea82b38cfe0c161545614eebde81e277efe53
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-11-21 20:45:31 +0900

    day 60 contact page

--- web_development/upgrade-my_blog/main.py
+++ web_development/upgrade-my_blog/main.py
@@ -1,6 +1,6 @@
 import requests
 from post import Post
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # 블로그 포스트 내용 API 제어 
 posts = requests.get("https://api.npoint.io/37689f9426dd018a0fd4").json()   # API 엔드포인트의 JSON 데이터 가져오기 
@@ -13,15 +13,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", all_posts=posts)
 
-# about page 
-@app.route('/about')
-def about_blog():
-    return render_template("about.html")
-
-# contact page 
-@app.route('/contact')
-def contact_me():
-    return render_template("contact.html")
 
 # 블로그 포스트 페이지 구조 
 @app.route('/post/<int:index>')
@@ -31,6 +22,27 @@ def blog_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+# about page 
+@app.route('/about')
+def about_blog():
+    return render_template("about.html")
+
+# contact form 
+@app.route('/contact', methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'POST':
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return "<h1>Successfully sent your message</h>"
+    
+    return render_template("contact.html")
+
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
