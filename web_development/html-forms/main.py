1bfc6e88d316f008324025b5f098cb32f8243d89
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-11-21 20:00:01 +0900

    day 60 Post method requests

--- web_development/html-forms/main.py
+++ web_development/html-forms/main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    # get data from form
+    username = request.form["username"]
+    password = request.form["password"]
+
+    return f"<h1>Name: {username}, Password: {password}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
