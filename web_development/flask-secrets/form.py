e6cb0ea24c1314039224b4578f09bbfa5adea8c5
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-11-26 22:22:19 +0900

    day 61 code refactoring for login logic

--- web_development/flask-secrets/form.py
+++ web_development/flask-secrets/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 
 # Flast-WTF으로 Login 양식 만들기 
 class LoginForm(FlaskForm):
-    email = StringField('Email')            # 유저명 - 이메일 형식(string)
-    password = PasswordField('Password')    # 비밀번호 - string 형식 
+    email = StringField(label='Email')            # 유저명 - 이메일 형식(string)
+    password = PasswordField(label='Password')    # 비밀번호 - string 형식 
+    submit = SubmitField(label='Log In')         # 로그인 버튼 
