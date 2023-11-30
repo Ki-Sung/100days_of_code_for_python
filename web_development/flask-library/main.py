ba1038f9dc5d26ab7b343f8eb662f479325105f1
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-11-30 19:00:34 +0900

    day 63 insert table and instert value in sqlite3

--- web_development/flask-library/main.py
+++ web_development/flask-library/main.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 ## 웹 서버 정의 
@@ -5,6 +6,21 @@ app = Flask(__name__)       # Flask 객체 선언
 
 ## book 정보 - Database 역할
 all_books = []
+
+## 데이터 베이스 정의 
+# 데이터베이스 연결 - 만약 데이터 베이스가 존재하지 않으면 생성
+db = sqlite3.connect("books-colleciton.db") 
+
+# 데이터 베이스 제어를 위해 커서 생성
+cursor = db.cursor()
+
+# 테이블 생성: books - id: int(Primary key) / title: varhcar(250), not null / author: varhcar(250), not null / rating: float, not null
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+# 데이터 추가 
+# cursor.execute("INSERT INTO books VALUES (1, 'Harry Potter', 'J. K. Rowling', 9.3)")
+# cursor.execute("INSERT INTO books VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', 4.7)")
+# db.commit()
 
 ## 모든 라우터를 정의
 
