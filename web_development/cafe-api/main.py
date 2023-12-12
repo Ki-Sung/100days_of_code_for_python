1f1dbe6afbd3927290ec978a70a21c5e412b1262
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-12-12 20:44:21 +0900

    day 66 replace codej for jsonify

--- web_development/cafe-api/main.py
+++ web_development/cafe-api/main.py
@@ -34,12 +34,19 @@ def home():
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    return jsonify(name=random_cafe.name,
-                   map_url=random_cafe.map_url,
-                   img_url=random_cafe.img_url,
-                   location=random_cafe.location,
-                   seats=random_cafe.seats,
-                   )
+    return jsonify(cafe = {
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price
+                })
 
 
 ## HTTP POST - Create Record
