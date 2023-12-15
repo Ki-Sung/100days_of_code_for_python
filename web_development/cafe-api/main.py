2acc7283867eafa3465f43fc235b14301989f4d4
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-12-15 19:47:20 +0900

    day 66 rafactorying for table configration

--- web_development/cafe-api/main.py
+++ web_development/cafe-api/main.py
@@ -4,13 +4,13 @@ import random
 
 app = Flask(__name__)
 
-##Connect to Database
+## 데이터 베이스 연결 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+## Cafe 테이블 구성
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -24,12 +24,21 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
 
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    
+## 모든 라우터 정의 
+# home page - url 체계: http://127.0.0.1:5000/
 @app.route("/")
 def home():
     return render_template("index.html")
 
-## HTTP GET - Read Record
+# HTTP GET - Read Record - random하게 카페 정보 조회 Page - url 체계: http://127.0.0.1:5000/random
 @app.route("/random")
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
@@ -48,24 +57,22 @@ def get_random_cafe():
         "coffee_price": random_cafe.coffee_price
                 })
 
+# HTTP GET - Read Record - 모든 카페 정보 조회 page - url 체계: http://127.0.0.1:5000/all
 @app.route("/all")
 def get_all_cafes():
     cafes = db.session.query(Cafe).all()
-    return jsonify(cafes = [
-        {
-            "id": cafe.id,
-            "name": cafe.name,
-            "map_url": cafe.map_url,
-            "img_url": cafe.img_url,
-            "location": cafe.location,
-            "seats": cafe.seats,
-            "has_toilet": cafe.has_toilet,
-            "has_wifi": cafe.has_wifi,
-            "has_sockets": cafe.has_sockets,
-            "can_take_calls": cafe.can_take_calls,
-            "coffee_price": cafe.coffee_price
-        } for cafe in cafes
-    ])
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+    
+@app.route("/search")
+def search_cafes():
+    # 1. 사용자가 입력한 검색어를 받아온다.
+    query_location = request.args.get("loc")
+    # 2. 검색어로 카페 이름을 찾아서 리스트에 담는다.
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
 
 ## HTTP POST - Create Record
 
