406889c6e0e03d3fdebdf48a92c93be473ad0cfe
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-12-15 23:54:45 +0900

    day 66 rafactorying for table configration

--- web_development/cafe-api/main.py
+++ web_development/cafe-api/main.py
@@ -24,50 +24,42 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    # mothod 정의 - cafe 객체를 딕셔너리로 변환하는 역할 (이렇게 하면 딕셔너리로 반환하여 JSON 형태로 반환하는 작업이 용이해짐)
     def to_dict(self):
-        dictionary = {}
-        for column in self.__table__.columns:
-            dictionary[column.name] = getattr(self, column.name)
-        return dictionary
+        # 방법 1
+        dictionary = {}                                             # 저장을 위한 빈 딕셔너리 생성 
+        for column in self.__table__.columns:                       # 테이블의 모든 column에 대한 정보를 가져와 loop 진행
+            dictionary[column.name] = getattr(self, column.name)    # 현재 객체에서의 column 값을 가져오기 
+        return dictionary                                           # 저장된 딕셔너리 반환
 
+        # 방법2 - 딕셔너리 구성하여 반환 
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
     
 ## 모든 라우터 정의 
 # home page - url 체계: http://127.0.0.1:5000/
 @app.route("/")
 def home():
+    # 지정한 url 체계로 index.html 템플릿 랜더링
     return render_template("index.html")
 
-# HTTP GET - Read Record - random하게 카페 정보 조회 Page - url 체계: http://127.0.0.1:5000/random
+# HTTP GET - Read Record 
+# random하게 카페 정보 조회 Page - url 체계: http://127.0.0.1:5000/random
 @app.route("/random")
 def get_random_cafe():
-    cafes = db.session.query(Cafe).all()
-    random_cafe = random.choice(cafes)
-    return jsonify(cafe = {
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price
-                })
+    cafes = db.session.query(Cafe).all()                            # SQLAlchemy를 사용하여 Cafe 모델에 대한 쿼리 개체 생성 후 모든 레코드 검색
+    random_cafe = random.choice(cafes)                              # random.choice 함수를 사용하여 모든 카페 목록에서 임의로 카페 선택 
+    return jsonify(cafe=random_cafe.to_dict())                      # 랜덤으로 선택된 카페 json 형식으로 변환 
 
-# HTTP GET - Read Record - 모든 카페 정보 조회 page - url 체계: http://127.0.0.1:5000/all
+# 모든 카페 정보 조회 page - url 체계: http://127.0.0.1:5000/all
 @app.route("/all")
 def get_all_cafes():
-    cafes = db.session.query(Cafe).all()
-    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
-    
+    cafes = db.session.query(Cafe).all()                            # SQLAlchemy를 사용하여 Cafe 모델에 대한 쿼리 개체 생성 후 모든 레코드 검색
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])        # 모든 카페 json 형식으로 변환
+
+# 위치 매개변수를 기반으로 카페를 검색하는 page - url 체계: http://127.0.0.1:5000/search?loc={cafe name}
 @app.route("/search")
 def search_cafes():
-    # 1. 사용자가 입력한 검색어를 받아온다.
     query_location = request.args.get("loc")
-    # 2. 검색어로 카페 이름을 찾아서 리스트에 담는다.
     cafe = db.session.query(Cafe).filter_by(location=query_location).first()
     if cafe:
         return jsonify(cafe=cafe.to_dict())
