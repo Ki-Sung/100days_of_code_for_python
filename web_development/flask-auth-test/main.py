031f95a3fe72e486d2c7ec3f3fe901a3e31f346a
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-12-29 23:21:27 +0900

    day 68 add register structure

--- web_development/flask-auth-test/main.py
+++ web_development/flask-auth-test/main.py
@@ -25,8 +25,20 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('secrets'))
+        
     return render_template("register.html")
 
 
